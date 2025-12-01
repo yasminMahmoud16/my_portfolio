2f61ccb735064b6eb0951391b1db476d9ea7d1f6
Yasmeen Mahmoud <yasmeen.mahmoud1610@gmail.com>
2025-12-01 17:29:15 +0200

    Using createHashRouter instade of createBrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import {  createHashRouter, RouterProvider } from "react-router-dom"
 import RouterLayout from "./Layout/RouterLayout.jsx"
 import Home from "./Pages/Home.jsx"
 import ThemeContextProvider from "./Context/ThemeContext.jsx";
 
 export default function App() {
-  const router = createBrowserRouter([
+  const router = createHashRouter([
     {
       path: "",
       element: <RouterLayout />,
