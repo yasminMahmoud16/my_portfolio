02ed50762ddfbf9991484d0a6a7a1fec958e9c1c
Yasmeen Mahmoud <yasmeen.mahmoud1610@gmail.com>
2025-12-12 16:30:54 +0200

    Adding projects links

--- src/Utils/Data.js
+++ src/Utils/Data.js
@@ -1,4 +1,3 @@
-
 import portfolio from "../assets/Images/portfolio.png"
 import goldenStitchBg from "../assets/Images/goldenStitchBg.png"
 import landingSurveybG from "../assets/Images/landingSurveybG.png"
@@ -36,319 +35,125 @@ import css from "../assets/Images/css-3-svgrepo-com.svg"
 import html from "../assets/Images/html-5-svgrepo-com.svg"
 
 
-
-
-
-
 export const projects = [
     {
-        "id": 11,
-        "name": "personal portfolio",
-        "link": "https://yasminmahmoud16.github.io/my_portfolio/",
-        "gitHubLink": "https://github.com/yasminMahmoud16/my_portfolio",
-        "image": portfolio,
-        "tech": [
-            "React",
-            "Ant design",
-            "Tailwind CSS",
-            "Ant design icons"
+        id: 11,
+        name: "personal portfolio",
+        link: "https://yasminmahmoud16.github.io/my_portfolio/",
+        gitHubLink: "https://github.com/yasminMahmoud16/my_portfolio",
+        behanceLink: "https://www.behance.net/gallery/240340935/portfolio",
+        image: portfolio,
+        tech: ["React", "Ant design", "Tailwind CSS", "Ant design icons"]
+    },
+    {
+        id: 1,
+        name: "golden Stitch Leathers",
+        link: "https://www.goldenstitchleathers.com/",
+        gitHubLink: "https://github.com/yasminMahmoud16/E-commerce-golden-stitch",
+        behanceLink:"https://www.behance.net/gallery/240340157/Golden-Stitch",
+        image: goldenStitchBg,
+        tech: [
+            "React", "TypeScript", "Shadcn UI", "Tailwind CSS", "Azios",
+            "TenStack/React-Query", "React-Hook-Form", "Zod", "jwt-decode", "react-icons"
         ]
     },
     {
-        "id": 1,
-        "name": "golden Stitch Leathers",
-        "link": "https://www.goldenstitchleathers.com/",
-        "gitHubLink": "https://github.com/yasminMahmoud16/E-commerce-golden-stitch",
-        "image": goldenStitchBg,
-        "tech": [
-            "React",
-            "TypeScript",
-            "Shadcn UI",
-            "Tailwind CSS",
-            "Azios",
-            "TenStack/React-Query",
-            "React-Hook-Form",
-            "Zod",
-            "jwt-decode",
-            "react-icons"
+        id: 2,
+        name: "landing Survey",
+        link: "https://surveyland.vercel.app/",
+        gitHubLink: "https://github.com/osamaashraf6/Landing-Survey",
+        image: landingSurveybG,
+        tech: [
+            "React", "Shadcn UI", "Tailwind CSS", "Azios", "React-Hook-Form", "yup", "jwt-decode", "react-icons"
         ]
     },
     {
-        "id": 2,
-        "name": "landing Survey",
-        "link": "https://surveyland.vercel.app/",
-        "gitHubLink": "https://github.com/osamaashraf6/Landing-Survey",
-        "image": landingSurveybG,
-        "tech": [
-            "React",
-            "Shadcn UI",
-            "Tailwind CSS",
-            "Azios",
-            "React-Hook-Form",
-            "yup",
-            "jwt-decode",
-            "react-icons"
+        id: 3,
+        name: "Ballance Full",
+        gitHubLink: "https://github.com/MostafaMnesey/BalanceFull",
+        behanceLink: "https://www.behance.net/gallery/229060637/Landing-Page",
+        image: balanceFullBg,
+        tech: ["React", "Flowbite", "Tailwind CSS", "Azios", "Formik", "Yup", "jwt-decode", "react-icons"]
+    },
+    {
+        id: 4,
+        name: "E-commerce App",
+        link: "https://yasminmahmoud16.github.io/E-commerceApp/#/login",
+        gitHubLink: "https://github.com/yasminMahmoud16/E-commerceApp",
+        behanceLink: "https://www.behance.net/gallery/219936697/E-commerce-APP",
+        image: ECommerceAppBg,
+        tech: [
+            "React", "Flowbite", "Tailwind CSS", "Azios", "TenStack/React-Query",
+            "Formik", "Yup", "jwt-decode", "Fontawesome"
         ]
     },
     {
-        "id": 3,
-        "name": "Ballance Full",
-        "gitHubLink": "https://github.com/MostafaMnesey/BalanceFull",
-        "behanceLink": "https://www.behance.net/gallery/229060637/Landing-Page",
-        "image": balanceFullBg,
-        "tech": [
-            "React",
-            "Flowbite",
-            "Tailwind CSS",
-            "Azios",
-            "Formik",
-            "Yup",
-            "jwt-decode",
-            "react-icons"
-
-        ]
+        id: 5,
+        name: "Meals App",
+        link: "https://yasminmahmoud16.github.io/mealsApp/",
+        gitHubLink: "https://github.com/yasminMahmoud16/mealsApp",
+        image: mealsAppBg,
+        tech: ["React", "Tailwind CSS", "Azios", "TenStack/React-Query"]
     },
     {
-        "id": 4,
-        "name": "E-commerce App",
-        "link": "https://yasminmahmoud16.github.io/E-commerceApp/#/login",
-        "gitHubLink": "https://github.com/yasminMahmoud16/E-commerceApp",
-        "behanceLink": "https://www.behance.net/gallery/219936697/E-commerce-APP",
-        "image": ECommerceAppBg,
-        "tech": [
-            "React",
-            "Flowbite",
-            "Tailwind CSS",
-            "Azios",
-            "TenStack/React-Query",
-            "Formik",
-            "Yup",
-            "jwt-decode",
-            "Fontawesome"
-        ]
+        id: 6,
+        name: "E-Book App",
+        link: "https://site3ebookstore.netlify.app/",
+        behanceLink: "https://www.behance.net/gallery/201031223/E-Book",
+        image: bookStoreBg,
+        tech: ["Javascript", "Bootstrap", "CSS-3", "HTML-5", "Responsive Design"]
     },
     {
-        "id": 5,
-        "name": "Meals App",
-        "link": "https://yasminmahmoud16.github.io/mealsApp/",
-        "gitHubLink": "https://github.com/yasminMahmoud16/mealsApp",
-        "image": mealsAppBg,
-        "tech": [
-            "React",
-            "Tailwind CSS",
-            "Azios",
-            "TenStack/React-Query"
-        ]
-    },
-    {
-        "id": 6,
-        "name": "E-Book App",
-        "link": "https://site3ebookstore.netlify.app/",
-        "behanceLink": "https://www.behance.net/gallery/201031223/E-Book",
-        "image": bookStoreBg,
-        "tech": [
-            "Javascript",
-            "Bootstrap",
-            "CSS-3",
-            "HTML-5",
-            "Responsive Design"
-        ]
-    },
-    {
-        "id": 7,
-        "name": "Grill-Restaurant App",
-        "link": "https://site6restaurant.netlify.app/",
-        "behanceLink": "https://www.behance.net/gallery/200953331/Grill-Restaurant",
-        "image": restaurantAppBg,
-        "tech": [
-            "Javascript",
-            "Bootstrap",
-            "CSS-3",
-            "HTML-5",
-            "Responsive Design"
-        ]
+        id: 7,
+        name: "Grill-Restaurant App",
+        link: "https://site6restaurant.netlify.app/",
+        behanceLink: "https://www.behance.net/gallery/200953331/Grill-Restaurant",
+        image: restaurantAppBg,
+        tech: ["Javascript", "Bootstrap", "CSS-3", "HTML-5", "Responsive Design"]
     }
 ];
 
 
 export const tools = {
-
-    "libraries": [
-        {
-            "id": 5,
-            "name": "ReactJs",
-            "image": react
-        },
-        {
-            "id": 6,
-            "name": "TailwindCss",
-            "image": tailwind
-        },
-        {
-            "id": 9,
-            "name": "shadcn",
-            "image": shadCn
-        },
-        {
-            "id": 90,
-            "name": "Material UI",
-            "image": MaterialUI
-        },
-        {
-            "id": 11,
-            "name": "Ant Design",
-            "image": AntDesign
-        },
-
-        {
-            "id": 10,
-            "name": "flowbite",
-            "image": flowbite
-        },
-        {
-            "id": 20,
-            "name": "framer-motion",
-            "image": framerMotion
-        },
-
-        {
-            "id": 7,
-            "name": "Axios",
-            "image": azios
-        },
-        {
-            "id": 8,
-            "name": "React Query",
-            "image": reactQuery
-        },
-        {
-            "id": 80,
-            "name": "Redux",
-            "image": redux
-        },
-        {
-            "id": 14,
-            "name": "React Hook Form",
-            "image": reactHookForm
-        },
-        {
-            "id": 15,
-            "name": "Formik",
-            "image": formik
-        },
-        {
-            "id": 16,
-            "name": "Zod",
-            "image": zod
-        },
-        {
-            "id": 17,
-            "name": "Joi",
-            "image": joi
-        },
-        {
-            "id": 18,
-            "name": "NodeJs",
-            "image": nodeJs
-        },
-        {
-            "id": 19,
-            "name": "ExpressJs",
-            "image": express
-        },
-        {
-            "id": 200,
-            "name": "MongoDB",
-            "image": mongoDb
-        },
-        {
-            "id": 28,
-            "name": "Mongoose.js",
-            "image": mongoose
-        },
-        {
-            "id": 29,
-            "name": "AWS",
-            "image": aws
-        },
-        {
-            "id": 21,
-            "name": "Figma",
-            "image": figma
-        },
-        {
-            "id": 210,
-            "name": "Git",
-            "image": git
-        },
-        {
-            "id": 211,
-            "name": "GitHub",
-            "image": github
-        },
-
-        {
-            "id": 12,
-            "name": "Bootstrap",
-            "image": bootstrap
-        }
+    libraries: [
+        { id: 5, name: "ReactJs", image: react },
+        { id: 6, name: "TailwindCss", image: tailwind },
+        { id: 9, name: "shadcn", image: shadCn },
+        { id: 90, name: "Material UI", image: MaterialUI },
+        { id: 11, name: "Ant Design", image: AntDesign },
+        { id: 10, name: "flowbite", image: flowbite },
+        { id: 20, name: "framer-motion", image: framerMotion },
+        { id: 7, name: "Axios", image: azios },
+        { id: 8, name: "React Query", image: reactQuery },
+        { id: 80, name: "Redux", image: redux },
+        { id: 14, name: "React Hook Form", image: reactHookForm },
+        { id: 15, name: "Formik", image: formik },
+        { id: 16, name: "Zod", image: zod },
+        { id: 17, name: "Joi", image: joi },
+        { id: 18, name: "NodeJs", image: nodeJs },
+        { id: 19, name: "ExpressJs", image: express },
+        { id: 200, name: "MongoDB", image: mongoDb },
+        { id: 28, name: "Mongoose.js", image: mongoose },
+        { id: 29, name: "AWS", image: aws },
+        { id: 21, name: "Figma", image: figma },
+        { id: 210, name: "Git", image: git },
+        { id: 211, name: "GitHub", image: github },
+        { id: 12, name: "Bootstrap", image: bootstrap }
     ],
-        "languages": [
-            {
-                "id": 1,
-                "name": "TypeScript",
-                "image": typescript
-            },
-            {
-                "id": 2,
-                "name": "JavaScript",
-                "image": javascript
-            },
-            {
-                "id": 3,
-                "name": "Css3",
-                "image": css
-            },
-            {
-                "id": 4,
-                "name": "Html5",
-                "image": html
-            }
-        ]
+    languages: [
+        { id: 1, name: "TypeScript", image: typescript },
+        { id: 2, name: "JavaScript", image: javascript },
+        { id: 3, name: "Css3", image: css },
+        { id: 4, name: "Html5", image: html }
+    ]
 };
 
 
 export const socialLinks = [
-    {
-        "icon": "github",
-        "type": "github",
-        "url": "https://github.com/yasminMahmoud16"
-    },
-    {
-        "icon": "linkedin",
-        "type": "linkedin",
-        "url": "https://www.linkedin.com/in/yasmeen-mahmoud-b5291422b/"
-    },
-    {
-        "icon": "behance",
-        "type": "behance",
-        "url": "https://www.behance.net/yasmeenmahmoud5"
-    },
-    {
-        "icon": "email",
-        "type": "email",
-        "url": "yasmeenmahmoud377@gmail.com"
-    },
-    {
-        "icon": "whatsApp",
-        "type": "whatsApp",
-        "url": "https://wa.me/+201033434141"
-    },
-    {
-        "icon": "phone",
-        "type": "phone",
-        "url": "01033434141"
-    }
+    { icon: "github", type: "github", url: "https://github.com/yasminMahmoud16" },
+    { icon: "linkedin", type: "linkedin", url: "https://www.linkedin.com/in/yasmeen-mahmoud-b5291422b/" },
+    { icon: "behance", type: "behance", url: "https://www.behance.net/yasmeenmahmoud5" },
+    { icon: "email", type: "email", url: "yasmeenmahmoud377@gmail.com" },
+    { icon: "whatsApp", type: "whatsApp", url: "https://wa.me/+201033434141" },
+    { icon: "phone", type: "phone", url: "01033434141" }
 ];
-
-
